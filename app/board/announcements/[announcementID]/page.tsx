5ecd8745810ff1384c7bb0bc7ca80eee3501cc1a
MicKorea Youtube <mickoreayt@gmail.com>
2024-04-08 02:07:42 +0000

    ㄹㅇ로 끝인 듯ㅋ

--- app/board/announcements/[announcementID]/page.tsx
+++ app/board/announcements/[announcementID]/page.tsx
@@ -25,32 +25,32 @@ export default function BoardSuggestionsReadPage({ params }: { params: { announc
 
   const classToAdd = "animate__fadeInUp"
 
-  const [suggestion, setSuggestion] = useState<DocumentData>()
+  const [announcement, setAnnouncement] = useState<DocumentData>()
 
   useEffect(() => {
     const unsubscribe = onSnapshot(doc(db, "announcements", params.announcementID), (querySnapshot) => {
-      setSuggestion(querySnapshot.data())
+      setAnnouncement(querySnapshot.data())
     })
   }, [params.announcementID])
 
   return (
     <>
       <section className="container py-20 md:px-40">
-        <InView triggerOnce={true} threshold={1}>
+        <InView triggerOnce={true} threshold={1} delay={1000}>
           {({ inView, ref }) => (
             <Card className={`animate__animated ${inView ? classToAdd : "invisible"}`} ref={ref}>
               <div className="flex justify-end">
                 <Link href="/board/announcements" className={buttonVariants({ variant: "ghost" }) + "font-SUITE-Regular px-2 absolute m-2"}><ChevronRight /></Link>
               </div>
               <CardHeader>
-                <CardTitle className="font-KBO-Dia-Gothic_bold text-2xl md:text-4xl">{suggestion?.title}</CardTitle>
+                <CardTitle className="font-KBO-Dia-Gothic_bold text-2xl md:text-4xl">{announcement?.title}</CardTitle>
                 <CardDescription className="font-SUITE-Regular text-md flex flex-col justify-between md:flex-row md:text-xl">
-                  <span>{suggestion?.author}</span>
-                  <span className="md:text-end">{suggestion?.updateTime.toDate().toLocaleString()}</span>
+                  <span>{announcement?.author}</span>
+                  <span className="md:text-end">{announcement?.updateTime.toDate().toLocaleString()}</span>
                 </CardDescription>
               </CardHeader>
               <CardContent className="font-SUITE-Regular space-y-3">
-                <p className="font-SUITE-Regular whitespace-pre-wrap text-lg">{suggestion?.content}</p>
+                <p className="font-SUITE-Regular whitespace-pre-wrap text-lg">{announcement?.content}</p>
               </CardContent>
               <CardFooter className="font-SUITE-Regular flex flex-col space-y-3">
                 <h1 className="text-md md:text-xl">댓글 기능은 추후에 제작 예정입니다.</h1>
