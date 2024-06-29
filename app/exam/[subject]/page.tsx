d0e582af517c1f5c7607bcbd2a5cc3c797be6718
leejunsibal <118183460+MicKoreaYoutube@users.noreply.github.com>
2024-06-29 05:12:04 +0000

    좆됨 방지용 커밋1

--- app/exam/[subject]/page.tsx
+++ app/exam/[subject]/page.tsx
@@ -34,6 +34,14 @@ import { z } from "zod"
 import { InView } from "react-intersection-observer"
 import { useState } from "react"
 
+interface questionListType {
+  unit: string
+  questionFormat: string
+  question: string
+  choices?: string[]
+  answer: string
+}
+
 export default function ExamDetail({ params }: { params: { subject: string } }) {
 
   return (
@@ -82,7 +90,68 @@ function SubjectIndicator({ subject }: { subject: string }) {
   const unitList = ["동백꽃", "양반전", "둘 다"]
   const questionFormatList = ["객관식 - 단어 맞추기", "주관식", "객관식 - 뜻 맞추기"]
   const [koreanState, setKoreanState] = useState("startPage")
-  let questionList = []
+  let questions: questionListType[] = []
+  const [questionList, setQuestionList] = useState<questionListType[]>([])
+  const wordList = {
+    "동백꽃": {
+      "횃소리": "날짐승이 크게 날갯짓을 하면서 탁탁 치는 소리.",
+      "실팍하다": "사람이나 물건 따위가 보기에 매우 실하다.",
+      "쌩이질": "한창 바쁠 때에 쓸데없는 일로 남을 귀찮게 구는 짓. 본말은 '씨양이질'.",
+      "수작": "남의 말이나 행동, 계획을 낮잡아 이르는 말.",
+      "할금할금": "곁눈으로 살그머니 계속 할겨 보는 모양.",
+      "생색": "다른 사람 앞에 당당히 나설 수 있거나 자랑할 수 있는 체면.",
+      "마름": "(옛날에) 땅 주인을 대신하여 농지를 관리하는 사람.",
+      "수군수군하다": "남이 알아듣지 못하도록 낮은 목소리로 가만가만 자꾸 이야기하다.",
+      "봉당": "안방과 건넌방 사이의 마루를 놓을 자리에 마루를 놓지 않고 흙바닥 그대로 둔 곳.",
+      "암팡스레": "몸은 작아도 야무지고 다부진 면이 있게.",
+      "풍기다": "어떤 분위기가 나다. 또는 그런 것을 자아내다.",
+      "서슬": "① 쇠붙이로 만든 연장이나 유리 조각 따위의 날카로운 부분. ② 강하고 날카로운 기세.",
+      "열벙거지": "'화(火)'를 속되게 이르는 말. 매우 급하게 치밀어 오르는 화증.",
+      "대거리": "상대편에게 맞서서 대듦. 또는 그런 말이나 행동.",
+      "두엄": "풀, 짚 또는 가축의 배설물 따위를 썩힌 거름.",
+      "감때사납다": "① 사람이 억세고 사납다. ② 사물이 험하고 거칠다.",
+      "하릴없이": "① 달리 어떻게 할 도리가 없이. ② 조금도 틀림이 없이.",
+      "뻐드러지다": "굳어서 뻣뻣하게 되다.",
+      "삭정이": "살아 있는 나무에 붙어 있는, 말라 죽은 가지.",
+      "싱둥겅둥": "건성건성, 정성을 들이지 않고 대강대강 일을 하는 모양.",
+      "호드기": "봄철에 물오른 버드나무 가지의 껍질을 고루 비틀어 뽑은 껍질이나 짤막한 밀짚 토막 따위로 만든 피리.",
+      "빈사지경(瀕死地境)": "거의 죽게 된 처지나 형편.",
+      "걱실걱실히": "성질이 너그러워 말과 행동을 시원스럽게 하는 모양.",
+      "알싸하다": "매운맛이나 독한 냄새 따위로 코 속이나 혀끝이 알알하다."
+    },
+    "양반전": {
+      "사족": "선비나 무인(人)의 집안. 또는 그 자손.",
+      "군수": "조선 시대에 둔, 지방 행정 단위인 군의 으뜸 벼슬.",
+      "환자": "환곡. 조선 시대에, 각 고을에서 봄에 백성들에게 곡식을 꾸어 주고 가을에 이자를 붙여 거두던 일. 또는 그 곡식.",
+      "섬": "부피의 단위, 곡식, 가루, 액체 따위의 부피를 잴 때 쓴다.",
+      "관찰사": "조선 시대에 둔, 각 도의 으뜸 벼슬.",
+      "순시하다": "돌아다니며 사정을 보살피다.",
+      "관아": "예전에, 벼슬아치들이 모여 나랏일을 처리하던 곳.",
+      "축내다": "① 일정한 수나 양에서 모자람이 생기게 하다. ② 몸이나 얼굴 따위에서 살이 빠지게 하다.",
+      "푼": "① 예전에, 엽전을 세던 단위. ② 돈을 세는 단위. 스스로 적은 액수라고 여길 때 쓴다.",
+      "비천하다": "지위나 신분이 낮고 천하다.",
+      "벙거지": "조선 시대에 궁중 또는 양반집의 하인이 쓰던 털로 만든 모자.",
+      "잠방이": "가랑이가 무릎까지 내려오도록 짧게 만든 홑바지.",
+      "소인": "신분이 낮은 사람이 자기보다 신분이 높은 사람에게 자기를 가리키는 말.",
+      "사사로이": "공적(公)이 아닌 개인적인 범위나 관계의 성질이 있게.",
+      "빌미": "재앙이나 탈 따위가 생기는 원인.",
+      "증서": "권리나 의무, 사실 따위를 증명하는 문서.",
+      "좌수": "조선시대에, 지방의 자치기구인 향청의 우두머리.",
+      "별감": "조선시대에, 향청에 속한 직책, 고을의 좌수에 버금가는 자리였다.",
+      "일절": "아주, 전혀, 절대로의 뜻으로, 흔히 행위를 그치게 하거나 어떤 일을 하지 않을 때에 쓰는 말.",
+      "오경": "하룻밤을 다섯 부분으로 나누었을 때 맨 마지막 부분. 새벽 세 시에서 다섯 시 사이이다.",
+      "마소": "말과 소를 아울러 이르는 말.",
+      "삼성": "오리온자리에 있는 별들. 중앙에 나란히 있는 세 개의 큰 별을 '삼형제별'이라 한다.",
+      "사농공상": "예전에, 백성을 나누던 네 가지 계급, 선비, 농부, 공장(匠), 상인을 이르던 말이다.",
+      "홍패": "문과 급제자에게 주던 합격증서.",
+      "음관": "과거를 거치지 아니하고 조상의 공덕에 의하여 맡은 벼슬. 또는 그런 벼슬아치.",
+      "일산": "햇볕을 가리기 위하여 세우는 큰 양산. 우산보다 크며 놀이할 때에 한데에다 세운다.",
+      "설렁줄": "처마 끝 같은 곳에 달아 놓은 방울을 울릴 때 잡아당기는 줄. 사람을 부를 때 쓴다.",
+      "부예지다": "살갗이나 얼굴 등이 허옇고 멀겋게 되다.",
+      "맹랑하다": "생각하던 바와 달리 허망하다."
+    }
+  }
+  const [currentQuestion, setCurrentQuestion] = useState()
 
   const examSchema = z.object({
     unitName: z.string({
@@ -118,10 +187,14 @@ function SubjectIndicator({ subject }: { subject: string }) {
   function submitExam(data: z.infer<typeof examSchema>) {
     console.log(data)
     setKoreanState("examPage")
+    const questionFormat = data.questionFormat[Math.floor(Math.random() * data.questionFormat.length)]
+    const unit = data.unitName == "둘 다" ? (Math.random() > 0.5 ? "동백꽃" : "양반전") : data.unitName
     for (let i = 1; i <= Number(data.questionNum); i++) {
-      questionList.push({
-        questionFormat: data.questionFormat[Math.floor(Math.random() * data.questionFormat.length)],
-        question: "하이!"
+      questions.push({
+        unit: unit,
+        questionFormat: questionFormat,
+        question: "하이!",
+        answer: ""
       })
     }
   }
